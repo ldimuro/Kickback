0ec4928ad5049f3d8773eb44cb09be39266738af
Lou DiMuro <ldimuro@asu.edu>
2018-08-05 19:37:00 -0700

    Started Add Station Page

--- Kickback/Controller/AddStationViewController.swift
+++ Kickback/Controller/AddStationViewController.swift
@@ -9,11 +9,42 @@
 import UIKit
 
 class AddStationViewController: UIViewController {
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var stationNameTextfield: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profilePicture.image = UserDataArray.profilePicture
+        
+        stationNameTextfield.becomeFirstResponder()
+        
+        let navBarHeight = UIApplication.shared.statusBarFrame.height +
+            self.navigationController!.navigationBar.frame.height
+        
+        let scrollViewHeight = self.view.frame.size.height - navBarHeight
+        print("SCROLLVIEW HEIGHT: \(scrollViewHeight)")
+        
+        let scrollViewDifference = self.view.frame.size.height - scrollViewHeight - 1
+        
+        print("HEIGHT: \(self.view.frame.size.height)\nSCROLL DIFFERENCE: \(scrollViewDifference)")
+        print(self.view.frame.size.height - scrollViewDifference)
+        
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height - scrollViewDifference)
+        
+        
+        let tapGestureBackground = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTapped(_:)))
+        self.view.addGestureRecognizer(tapGestureBackground)
 
-        // Do any additional setup after loading the view.
+    }
+    
+    @objc func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        self.isEditing = false
+        stationNameTextfield.resignFirstResponder()
+        print("background tapped")
     }
 
     @IBAction func createStationButton(_ sender: Any) {
@@ -22,7 +53,13 @@ class AddStationViewController: UIViewController {
         
     }
     
-
+    @IBAction func cancelButton(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
